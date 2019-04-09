092ac1e29bf52d32342b927de5c3ea072a90fa09
Björn Hempel <bjoern@hempel.li>
2019-04-09 13:12:02 +0200

    remove iml files from repository

--- src/main/java/classification/Test.java
+++ src/main/java/classification/Test.java
@@ -4,11 +4,9 @@ import ws.palladian.retrieval.DocumentRetriever;
 
 public class Test {
     public static void main(String[] args) {
-        DocumentRetriever documentRetriever = new DocumentRetriever();
-
-        documentRetriever.test();
-
+        //DocumentRetriever documentRetriever = new DocumentRetriever();
+        String text = "123"; //documentRetriever.test();
         //String text = documentRetriever.getText("https://www.google.de");
-        //System.out.println(text);
+        System.out.println(text);
     }
 }
